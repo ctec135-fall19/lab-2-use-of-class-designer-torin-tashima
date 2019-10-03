b2288b2c7feffd99e824d7cee4ced48b4142e1c7
IndieFox98 <ttashima7@gmail.com>
2019-10-02 22:04:43 -0700

    Added Car and SportsCar class and updated Main.

---
+++ MySecondSolution/MySecondProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySecondProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Player {0}: What The Hello World?", i + 1);
+            }
+
+            SportsCar car = new SportsCar();
+            var carName = car.GetPetName();
+            Console.WriteLine("My car's name is {0}.", carName);
+        }
+    }
+}
---
+++ MySecondSolution/MySecondProject/SportsCar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MySecondProject
+{
+    public class SportsCar : Car
+    {
+        public string GetPetName()
+        {
+            PetName = "Fred";
+            return PetName;
+        }
+    }
+}
